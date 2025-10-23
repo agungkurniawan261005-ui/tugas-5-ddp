ccbb5737a588dd7f4cdcc88ff6fa807bf1a9e4a1
agungkurniawan261005-ui <agungkurniawan261005@gmail.com>
2025-10-23 15:43:21 +0700

    Add files via upload

---
+++ soal-1.py
@@ -0,0 +1,4 @@
+kendaraan=["astrea","motor","100cc","hitam","roda dua"]
+kendaraan.extend(["5 juta","motor"])
+kendaraan.insert(2,"honda")
+print(kendaraan)
---
+++ soal-2.py
@@ -0,0 +1,22 @@
+print("program menghitung luas bangun datar")
+print("1. persegi")
+print("2. lingkaran")
+print("3. segitiga")
+
+hitung=int(input("memilih program (1/2/3)"))
+match hitung:
+    case 1 :
+        sisi=float(input("masukkan panjang sisi : "))
+        luas=sisi*sisi
+        print(luas)
+    case 2 :
+        r=float(input("masukkan jari-jari lingkaran : "))
+        luas=3.14*r*r
+        print(luas)
+    case 3 :
+        alas=float(input("masukkan alas segitiga : "))
+        tinggi=float(input("masukkan tinggi segitiga : "))
+        luas=0.5*alas*tinggi
+        print(luas)
+    case _ :
+        print("SALAH PILIH")
